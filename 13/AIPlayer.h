145581a89d09fdcb003c127752104c5262b74eee
Parker <pakalewis@gmail.com>
2015-03-12 21:00:31 -0700

    New class for AIPlayer

---
+++ 13/AIPlayer.h
@@ -0,0 +1,13 @@
+//
+//  AIPlayer.h
+//  13
+//
+//  Created by Parker Lewis on 3/12/15.
+//  Copyright (c) 2015 Parker Lewis. All rights reserved.
+//
+
+#import "Player.h"
+
+@interface AIPlayer : Player
+
+@end
---
+++ 13/AIPlayer.m
@@ -0,0 +1,13 @@
+//
+//  AIPlayer.m
+//  13
+//
+//  Created by Parker Lewis on 3/12/15.
+//  Copyright (c) 2015 Parker Lewis. All rights reserved.
+//
+
+#import "AIPlayer.h"
+
+@implementation AIPlayer
+
+@end
